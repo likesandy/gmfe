757d2b72e8fb73415a534be12bb0831861d8cc7d
tao <1445709137@qq.com>
2024-06-20 11:04:01 +0800

    feat: ✨ 图片预览组件

---
+++ packages/react/src/component/picture_preview/index.js
@@ -0,0 +1,22 @@
+import 'react-photo-view/dist/react-photo-view.css'
+import { PhotoProvider, PhotoView } from 'react-photo-view'
+import PropTypes from 'prop-types'
+import React from 'react'
+import './style.less'
+
+export default function PicturePreview({ images, className }) {
+  return (
+    <PhotoProvider>
+      {(images || []).map((item, index) => (
+        <PhotoView key={index} src={item}>
+          <img src={item} alt='' className={className} />
+        </PhotoView>
+      ))}
+    </PhotoProvider>
+  )
+}
+
+PicturePreview.propTypes = {
+  images: PropTypes.array,
+  className: PropTypes.string
+}
